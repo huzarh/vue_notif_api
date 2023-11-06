1213e3e12277c686e1ce48bceeb734b5ac5108b5
huzarh <huzarh44@gmail.com>
2023-11-06 13:49:12 +0300

    test 2

--- dist/config/db.js
+++ dist/config/db.js
@@ -4,10 +4,8 @@ const mongoose = require("mongoose");
 const MONGODB_URI = 'mongodb+srv://huzarh44:kKmcQRCFFeJvAvyV@cluster0.occre5s.mongodb.net';
 const connectDB = async () => {
   const conn = await mongoose.connect(MONGODB_URI, {
-    serverSelectionTimeoutMS: 5000,
-    socketTimeoutMS: 30000,
-    maxIdleTimeMS: 30000,
-    connectTimeoutMS: 30000
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
   console.log(`MongoDB is connected: ${conn.connection.host}`);
 };
